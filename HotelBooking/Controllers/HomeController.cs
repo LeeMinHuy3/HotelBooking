e112b42f4e8c241e51072baee13256d449295da6
LeeMinHuy3 <leminhuy3@gmail.com>
2025-04-11 01:09:59 +0700

    Sửa đổi một vài vị trí của frontend

--- HotelBooking/Controllers/HomeController.cs
+++ HotelBooking/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace HotelBooking.Controllers
             return View();
         }
 
+        public IActionResult GioiThieu()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
