b4a180730b1e8bfb28555e6e9ccf91a7ae29ed94
LeeMinHuy3 <leminhuy3@gmail.com>
2025-04-11 02:00:34 +0700

    sửa một số lỗi ở frontend

--- HotelBooking/Controllers/DichVuController.cs
+++ HotelBooking/Controllers/DichVuController.cs
@@ -1,12 +1,102 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelBooking.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HotelBooking.Controllers
 {
     public class DichVuController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext _context;
+
+        public DichVuController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: DichVu
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.DichVus.ToListAsync());
+        }
+
+        // GET: DichVu/Create
+        public IActionResult Create()
         {
             return View();
+        }
+
+        // POST: DichVu/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MaDV,KieuDichVu,Gia")] DichVu dichVu)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(dichVu);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(dichVu);
+        }
+
+        // GET: DichVu/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var dichVu = await _context.DichVus.FindAsync(id);
+            if (dichVu == null) return NotFound();
+
+            return View(dichVu);
+        }
+
+        // POST: DichVu/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("MaDV,KieuDichVu,Gia")] DichVu dichVu)
+        {
+            if (id != dichVu.MaDV) return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(dichVu);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!_context.DichVus.Any(e => e.MaDV == id))
+                        return NotFound();
+                    else
+                        throw;
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(dichVu);
+        }
+
+        // GET: DichVu/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var dichVu = await _context.DichVus
+                .FirstOrDefaultAsync(m => m.MaDV == id);
+            if (dichVu == null) return NotFound();
+
+            return View(dichVu);
+        }
+
+        // POST: DichVu/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var dichVu = await _context.DichVus.FindAsync(id);
+            _context.DichVus.Remove(dichVu);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
